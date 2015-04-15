e8a1cfdd63ad162ccbded662cb327851ab390ebd
frontainer <frontainer@gmail.com>
2015-04-15 22:32:05 +0900

    empty.scssの読み込みテストを追加

--- test/file/file.js
+++ test/file/file.js
@@ -1,11 +1,13 @@
 var assert = require('power-assert');
 var File = require('../../lib/file/file');
 
+var emptyText = '// empty\n.test {\n    font-size: 14px;\n}';
 var sampleText = '/*\n#styleguide\nstyle title\n\nstyle comment.\n\n@depulicated\n@非推奨\n@todo\n@your-attribute\n\n```\nsample code here.\n```\n*/\n\n/*\n#colors\n\n@primary #996600\n@secondary #333\n@color-name color-code\n*/';
+
 
 module.exports = function() {
     describe('File', function() {
-        var files = ["test/sass/sample.scss","test/sass/sample2.scss"];
+        var files = ["test/sass/empty.scss","test/sass/sample.scss","test/sass/sample2.scss"];
         beforeEach(function() {
 
         });
@@ -17,8 +19,9 @@ module.exports = function() {
         });
         it('getAllFiles', function(done) {
             File.getAllFiles(files,function(err,result) {
-                assert(result.length === 2);
-                assert(result[0].content === sampleText);
+                assert(result.length === 3);
+                assert(result[0].content === emptyText);
+                assert(result[1].content === sampleText);
                 done();
             });
         });
