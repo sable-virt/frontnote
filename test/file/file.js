fafeab651edfecc95b147ae7d002ab4124987de1
frontainer <frontainer@gmail.com>
2015-04-18 20:18:04 +0900

    overviewテストを追加とテスト項目を簡易化

--- test/file/file.js
+++ test/file/file.js
@@ -2,12 +2,12 @@ var assert = require('power-assert');
 var File = require('../../lib/file/file');
 
 var emptyText = '// empty\n.test {\n    font-size: 14px;\n}';
-var sampleText = '/*\n#styleguide\nstyle title\n\nstyle comment.\n\n@depulicated\n@非推奨\n@todo\n@your-attribute\n\n```\nsample code here.\n```\n*/\n\n/*\n#colors\n\n@primary #996600\n@secondary #333\n@color-name color-code\n*/';
+var sampleText = '/*\n#overview\ntitle\n\noverview comment\n*/\n\n/*\n#styleguide\nstyle title\n\nstyle comment.\n\n@depulicated\n@非推奨\n@todo\n@your-attribute\n\n```\nsample code here.\n```\n*/\n\n/*\n#colors\n\n@primary #996600\n@secondary #333\n@color-name color-code\n*/';
 
 
 module.exports = function() {
     describe('File', function() {
-        var files = ["test/sass/empty.scss","test/sass/sample.scss","test/sass/sample2.scss"];
+        var files = ["test/sass/empty.scss","test/sass/sample.scss"];
         beforeEach(function() {
 
         });
@@ -19,7 +19,7 @@ module.exports = function() {
         });
         it('getAllFiles', function(done) {
             File.getAllFiles(files,function(err,result) {
-                assert(result.length === 3);
+                assert(result.length === 2);
                 assert(result[0].content === emptyText);
                 assert(result[1].content === sampleText);
                 done();
