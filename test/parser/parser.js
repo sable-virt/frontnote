60120ed7c9902da9127d67e35d79ff4f6a4d1534
frontainer <frontainer@gmail.com>
2015-04-18 16:57:00 +0900

    overviewのテスト追加

--- test/parser/parser.js
+++ test/parser/parser.js
@@ -4,13 +4,16 @@ var Parser = require('../../lib/parser/parser');
 var sampleFile = '../test/sass/sample.scss';
 var sampleText = '/*\n#styleguide\nstyle title\n\nstyle comment.\n\n@depulicated\n@非推奨\n@todo\n@your-attribute\n\n```\nsample code here.\n```\n*/\n\n/*\n#colors\n\n@primary #996600\n@secondary #333\n@color-name color-code\n*/';
 
+var sampleFile2 = '../test/sass/sample2.scss';
+var sampleText2 = '/*\n#overview\ntest\n\n## overview title\n*/';
+
 module.exports = function() {
     describe('Parser', function() {
         var parser;
         beforeEach(function() {
             parser = new Parser();
         });
-        it('parse', function(done) {
+        it('parse', function() {
             var result = parser.parse(sampleFile,sampleText);
             assert(result.fileName === 'sample');
             assert(result.url === 'test-sass-sample.html');
@@ -29,8 +32,24 @@ module.exports = function() {
 
             assert(result.colors[1].name === 'secondary');
             assert(result.colors[1].color === '#333');
-            //console.log(result.colors);
-            done();
+        });
+
+        it('parse overview', function() {
+            var result = parser.parse(sampleFile2,sampleText2);
+            assert(result.fileName === 'sample2');
+            assert(result.url === 'test-sass-sample2.html');
+
+            assert(result.ext === '.scss');
+            assert.deepEqual(result.dirs,['..','test','sass','sample2.scss']);
+
+            assert(result.sections === null);
+
+            assert(result.overview.title === 'test');
+            assert(result.overview.comment === '## overview title');
+            assert.deepEqual(result.overview.attributes,[]);
+            assert(result.overview.code === null);
+
+            assert(result.colors === null);
         });
     });
 };
