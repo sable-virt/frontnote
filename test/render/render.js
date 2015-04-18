e69eda2dbc04a358ba51999f78fe098011a4a22b
frontainer <frontainer@gmail.com>
2015-04-18 21:46:43 +0900

    ベタ書きになっていたのをejsテンプレートに置き換え

--- test/render/render.js
+++ test/render/render.js
@@ -9,13 +9,13 @@ module.exports = function() {
             render = new Render();
         });
         it('generateIncludeCss', function() {
-            assert(render.generateIncludeCss('main.css'),'<link rel="stylesheet" href="main.css"/>');
+            assert(render.generateIncludeCss('main.css'),'<link rel="stylesheet" href="main.css" />');
             var css = render.generateIncludeCss([
                 'main.css',
                 'style.css',
                 'sub.css'
             ]);
-            assert(css == '<link rel="stylesheet" href="main.css"/>\n<link rel="stylesheet" href="style.css"/>\n<link rel="stylesheet" href="sub.css"/>');
+            assert(css == '<link rel="stylesheet" href="main.css" />\n<link rel="stylesheet" href="style.css" />\n<link rel="stylesheet" href="sub.css" />');
         });
         it('generateIncludeScript', function() {
             assert(render.generateIncludeScript('main.js'),'<script src="main.js"></script>');
